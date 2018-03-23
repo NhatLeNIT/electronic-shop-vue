867818f632ee46303f1b2b26413324fffe7494cb
MasterNew <tanhoangle1996@gmail.com>
2018-03-24 00:04:55 +0700

    tan - header + footer

---
+++ src/components/shared/Header.vue
@@ -0,0 +1,227 @@
+<template>
+    <div id="header">
+        <div class="header">
+            <div class="top-header top-header4">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-6 col-sm-6 col-xs-12">
+                            <div class="account-login account-login3">
+                                <a href="#">My Account</a>
+                                <a href="#">checkout</a>
+                                <a href="#">Login</a>
+                                <a href="#">Register</a>
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-sm-6 hidden-xs">
+                            <div class="currency-language currency-language3">
+                                <div class="language-box">
+                                    <a href="#" class="language-current">
+                                        <img src="./../../assets/images/flag/flag.png" alt="">
+                                        <span>English</span>
+                                    </a>
+                                    <ul class="language-list list-unstyled">
+                                        <li>
+                                            <a href="#">
+                                                <img src="./../../assets/images/flag/flag-en.png" alt="">
+                                                <span>English</span>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                <img src="./../../assets/images/flag/flag-fr.png" alt="">
+                                                <span>French</span>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                <img src="./../../assets/images/flag/flag-gm.png" alt="">
+                                                <span>German</span>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="currency-box">
+                                    <a href="#" class="currency-current">
+                                        <span>USD</span>
+                                    </a>
+                                    <ul class="currency-list list-unstyled">
+                                        <li>
+                                            <a href="#">
+                                                <span class="currency-index">€</span>EUR</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                <span class="currency-index">¥</span>JPY</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                <span class="currency-index">$</span>USD</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- end top header -->
+            <div class="main-header2 main-header12">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-3 col-sm-3 col-xs-12">
+                            <div class="logo logo2">
+                                <h1 class="hidden">Kute Shop Html Theme</h1>
+                                <a href="index.html">
+                                    <img src="./../../assets/images/home12/logo.png" alt="" />
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-md-5 col-sm-6 col-xs-12">
+                            <div class="wrap-search1">
+                                <div class="smart-search smart-search2">
+                                    <div class="select-category">
+                                        <a class="category-toggle-link" href="#">
+                                            <span>All Categories</span>
+                                        </a>
+                                        <ul class="list-category-toggle list-unstyled">
+                                            <li>
+                                                <a href="#">Computer &amp; Office</a>
+                                            </li>
+                                            <li>
+                                                <a href="#">Elextronics</a>
+                                            </li>
+                                            <li>
+                                                <a href="#">Jewelry &amp; Watches</a>
+                                            </li>
+                                            <li>
+                                                <a href="#">Home &amp; Garden</a>
+                                            </li>
+                                            <li>
+                                                <a href="#">Bags &amp; Shoes</a>
+                                            </li>
+                                            <li>
+                                                <a href="#">Kids &amp; Baby</a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <form class="smart-search-form">
+                                        <input type="text" onblur="if (this.value=='') this.value = this.defaultValue" onfocus="if (this.value==this.defaultValue) this.value = ''"
+                                            value="Search...">
+                                        <div class="submit-form">
+                                            <input class="radius" type="submit" value="">
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4 col-sm-3 col-xs-8">
+                            <div class="cart-social clearfix">
+                                <div class="mini-cart-box mini-cart1">
+                                    <a class="mini-cart-link" href="cart.html">
+                                        <span class="mini-cart-icon">
+                                            <i class="fa fa-shopping-basket" aria-hidden="true"></i>
+                                        </span>
+                                        <span class="mini-cart-number border radius">0</span>
+                                    </a>
+                                    <div class="mini-cart-content">
+                                        <h2>(2) ITEMS IN MY CART</h2>
+                                        <ul class="list-mini-cart-item list-unstyled">
+                                            <li>
+                                                <div class="mini-cart-edit">
+                                                    <a class="delete-mini-cart-item" href="#">
+                                                        <i class="fa fa-trash-o"></i>
+                                                    </a>
+                                                    <a class="edit-mini-cart-item" href="#">
+                                                        <i class="fa fa-pencil"></i>
+                                                    </a>
+                                                </div>
+                                                <div class="mini-cart-thumb">
+                                                    <a href="#">
+                                                        <img alt="" src="./../../assets/images/home1/mini-cart-thumb.png">
+                                                    </a>
+                                                </div>
+                                                <div class="mini-cart-info">
+                                                    <h3>
+                                                        <a href="#">Burberry Pink &amp; black</a>
+                                                    </h3>
+                                                    <div class="info-price">
+                                                        <span>$59.52</span>
+                                                        <del>$17.96</del>
+                                                    </div>
+                                                    <div class="qty-product">
+                                                        <span class="qty-down">-</span>
+                                                        <span class="qty-num">1</span>
+                                                        <span class="qty-up">+</span>
+                                                    </div>
+                                                </div>
+                                            </li>
+                                            <li>
+                                                <div class="mini-cart-edit">
+                                                    <a class="delete-mini-cart-item" href="#">
+                                                        <i class="fa fa-trash-o"></i>
+                                                    </a>
+                                                    <a class="edit-mini-cart-item" href="#">
+                                                        <i class="fa fa-pencil"></i>
+                                                    </a>
+                                                </div>
+                                                <div class="mini-cart-thumb">
+                                                    <a href="#">
+                                                        <img alt="" src="./../../assets/images/home1/mini-cart-thumb.png">
+                                                    </a>
+                                                </div>
+                                                <div class="mini-cart-info">
+                                                    <h3>
+                                                        <a href="#">Burberry Pink &amp; black</a>
+                                                    </h3>
+                                                    <div class="info-price">
+                                                        <span>$59.52</span>
+                                                        <del>$17.96</del>
+                                                    </div>
+                                                    <div class="qty-product">
+                                                        <span class="qty-down">-</span>
+                                                        <span class="qty-num">1</span>
+                                                        <span class="qty-up">+</span>
+                                                    </div>
+                                                </div>
+                                            </li>
+                                        </ul>
+                                        <div class="mini-cart-total">
+                                            <label>TOTAL</label>
+                                            <span>$24.28</span>
+                                        </div>
+                                        <div class="mini-cart-button">
+                                            <a class="mini-cart-view" href="#">view my cart </a>
+                                            <a class="mini-cart-checkout" href="#">Checkout</a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- End Mini Cart -->
+                                <div class="social-header style2">
+                                    <a href="#">
+                                        <i class="fa fa-user" aria-hidden="true"></i>
+                                    </a>
+                                    <a href="#">
+                                        <i class="fa fa-heart" aria-hidden="true"></i>
+                                    </a>
+                                    <a href="#">
+                                        <i class="fa fa-refresh" aria-hidden="true"></i>
+                                    </a>
+                                    <a href="#">
+                                        <i class="fa fa-check-square-o" aria-hidden="true"></i>
+                                    </a>
+                                </div>
+                                <!-- End Social -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- end main header -->
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+};
+</script>
